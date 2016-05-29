4fb91998085dd33157e1a46989eeddf7d750ad1e
Juan Lanfranco Sanchez <jlanfrancos@gmail.com>
2016-05-29 12:59:05 -0500

    Agregando Proyecto Java Swing

---
+++ HolaMundoSwing/src/pe/lanfranco/app/model/Mensaje.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.lanfranco.app.model;
+
+/**
+ *
+ * @author Juan
+ */
+public class Mensaje {
+    
+    private String mensaje;
+
+    public Mensaje() {
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
